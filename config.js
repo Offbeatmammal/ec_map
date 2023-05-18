766cd63e44bd8ce8ea09ecae231e5d00dbbb08e8
Offbeatmammal <offbeatmammal@gmail.com>
2023-05-18 11:08:50 +1000

    Initial commit

---
+++ config.js
@@ -0,0 +1,16 @@
+circuits = [["Phillip Island","TrackMapPhillipIsland.jpg"],["Sandown","TrackMapSandown.jpg"],["Sandown-Alt","TrackMapSandown2.png"]]
+styles = [
+    ["icon-size",'2vw'],
+    ["button-font-weight",'400'],
+    ["button-font-size",'14px'],
+    ["msg-font-weight",'400'],
+    ["msg-font-size",'14px'],
+    
+    ["cr-bgcolor",'blue'],["cr-color",'white'],
+    ["fire-bgcolor",'red'],["fire-color",'white'],
+    ["tow-bgcolor",'yellow'],["tow-color",'black'],
+    ["victor-bgcolor",'green'],["victor-color",'white'],
+    ["service-bgcolor",'lightblue'],["service-color",'white'],
+    ["buggy-bgcolor",'lightslategray'],["buggy-color",'white'],
+    ["crash-bgcolor",'rgb(255, 153, 0)'],["crash-color",'black']
+]
