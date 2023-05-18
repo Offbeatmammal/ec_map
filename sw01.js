4ece89c5a76f3e97518ec11c54aefdac9bfd67f2
Offbeatmammal <offbeatmammal@gmail.com>
2023-05-18 19:37:16 +1000

    fix for PWA

--- sw01.js
+++ sw01.js
@@ -1,15 +1,24 @@
 // Choose a cache name
 const cacheName = 'cache-v1';
 // List the files to precache
-const precacheResources = ['/','./index.html','./styles.css','./config.js', "./manifest.json",
+const precacheResources = ['./index.html','./styles.css','./config.js', "./manifest.json",
 './js/jquery-3.7.0.min.js','./js/jquery-ui.min.js',
 './icons/ambulance.png','./icons/buggy.png','./icons/crash.png','./icons/favicon.ico','./icons/fire.png','./icons/flatbed.png','./icons/jeep.png','./icons/service.png','./icons/trash.png',
-'./tracks/TrackMapPhillipIsland.jpg','./tracks/TrackMapSandown.jpg','./tracks/TrackMapSandown2.jpg'];
+'./tracks/TrackMapPhillipIsland.jpg','./tracks/TrackMapSandown.jpg','./tracks/TrackMapSandown2.png'];
 
 // When the service worker is installing, open the cache and add the precache resources to it
 self.addEventListener('install', (event) => {
   //console.log('Service worker install event!');
-  event.waitUntil(caches.open(cacheName).then((cache) => cache.addAll(precacheResources)));
+  //event.waitUntil(caches.open(cacheName).then((cache) => cache.addAll(precacheResources)).catch(err => console.log(err)));
+
+  event.waitUntil(caches.open(cacheName).then((cache) => {
+    const stack = [];
+    precacheResources.forEach(file => stack.push(
+        cache.add(file).catch(_=>console.error(`can't load ${file} to cache`))
+    ));
+    return Promise.all(stack);
+}));
+
 });
 
 self.addEventListener('activate', (event) => {
